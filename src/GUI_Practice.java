1d8332dfda109be730305422f21e2bc519c4501c
David Lara <david.a.lara05@gmail.com>
2022-11-04 17:18:41 -0500

    did some gui practice

--- src/GUI_Practice.java
+++ src/GUI_Practice.java
@@ -1,2 +1,16 @@
+import javax.swing.JOptionPane;
+
 public class GUI_Practice {
-}
+    public static void main(String[] args) {
+
+        String name = JOptionPane.showInputDialog("What is your name");
+        JOptionPane.showMessageDialog(null, "Hello " + name);
+
+        int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age"));
+        JOptionPane.showMessageDialog(null, "You are " + age + " years old");
+
+        double height = Double.parseDouble(JOptionPane.showInputDialog("Enter your height"));
+        JOptionPane.showMessageDialog(null, "You are " + height);
+
+    } // end of main
+} // end of class
